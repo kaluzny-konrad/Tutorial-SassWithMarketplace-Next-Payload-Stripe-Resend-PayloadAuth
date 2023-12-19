160273c0f081fa366072b05381aa38ee58b0ee5c
Konrad Kałużny <konrad.kaluzny@hotmail.com>
2023-12-19 18:10:19 +0100

    Add environment variable and update dependencies

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { getPayloadClient } from "@/get-payload";
+import { getPayloadClient } from "./get-payload";
 import { nextApp, nextHandler } from "./next-utils";
 
 const app = express();
