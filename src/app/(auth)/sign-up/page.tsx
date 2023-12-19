73686260656872e042acebc126e6718d57bca2a1
Konrad Kałużny <konrad.kaluzny@hotmail.com>
2023-12-19 18:39:13 +0100

    Add sign-up page with form validation

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,9 +1,99 @@
-import React from 'react'
+'use client'
 
-type Props = {}
+import {
+  AuthCredentialsValidator,
+  TAuthCredentialsValidator,
+} from "@/lib/validators/account-credentials-validator";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { cn } from "@/lib/utils";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { Icons } from "@/components/Icons";
+import Link from "next/link";
+import { ArrowRightIcon } from "lucide-react";
+
+type Props = {};
 
 export default function page({}: Props) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<TAuthCredentialsValidator>({
+    resolver: zodResolver(AuthCredentialsValidator),
+  });
+
+  const onSubmit = ({ email, password }: TAuthCredentialsValidator) => {
+    // mutate({ email, password })
+  };
+
   return (
-    <div>page</div>
+    <>
+      <div className='container relative flex pt-20 flex-col items-center justify-center lg:px-0'>
+        <div className='mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]'>
+          <div className='flex flex-col items-center space-y-2 text-center'>
+            <Icons.logo className='h-20 w-20' />
+            <h1 className='text-2xl font-semibold tracking-tight'>
+              Create an account
+            </h1>
+
+            <Link
+              className={buttonVariants({
+                variant: 'link',
+                className: 'gap-1.5',
+              })}
+              href='/sign-in'>
+              Already have an account? Sign-in
+              <ArrowRightIcon className='h-4 w-4' />
+            </Link>
+          </div>
+
+          <div className='grid gap-6'>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className='grid gap-2'>
+                <div className='grid gap-1 py-2'>
+                  <Label htmlFor='email'>Email</Label>
+                  <Input
+                    {...register('email')}
+                    className={cn({
+                      'focus-visible:ring-red-500':
+                        errors.email,
+                    })}
+                    placeholder='you@example.com'
+                  />
+                  {errors?.email && (
+                    <p className='text-sm text-red-500'>
+                      {errors.email.message}
+                    </p>
+                  )}
+                </div>
+
+                <div className='grid gap-1 py-2'>
+                  <Label htmlFor='password'>Password</Label>
+                  <Input
+                    {...register('password')}
+                    type='password'
+                    className={cn({
+                      'focus-visible:ring-red-500':
+                        errors.password,
+                    })}
+                    placeholder='Password'
+                  />
+                  {errors?.password && (
+                    <p className='text-sm text-red-500'>
+                      {errors.password.message}
+                    </p>
+                  )}
+                </div>
+
+                <Button>Sign up</Button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
