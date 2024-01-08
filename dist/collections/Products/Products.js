94edb11ba1397a58430dbc0ea71b804308d06b90
Konrad Kałużny <konrad.kaluzny@hotmail.com>
2024-01-08 18:25:16 +0100

    migrate to npm

--- dist/collections/Products/Products.js
+++ dist/collections/Products/Products.js
@@ -46,6 +46,15 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Products = void 0;
 var config_1 = require("../../config");
@@ -60,49 +69,110 @@ var addUser = function (_a) {
         });
     });
 };
+var syncUser = function (_a) {
+    var req = _a.req, doc = _a.doc;
+    return __awaiter(void 0, void 0, void 0, function () {
+        var fullUser, products, allProductIds_1, createdProductIds, dataToUpdate;
+        return __generator(this, function (_b) {
+            switch (_b.label) {
+                case 0: return [4 /*yield*/, req.payload.findByID({
+                        collection: "users",
+                        id: req.user.id,
+                    })];
+                case 1:
+                    fullUser = _b.sent();
+                    if (!(fullUser && typeof fullUser === "object")) return [3 /*break*/, 3];
+                    products = fullUser.products;
+                    allProductIds_1 = __spreadArray([], ((products === null || products === void 0 ? void 0 : products.map(function (product) {
+                        return typeof product === "object" ? product.id : product;
+                    })) || []), true);
+                    createdProductIds = allProductIds_1.filter(function (id, index) { return allProductIds_1.indexOf(id) === index; });
+                    dataToUpdate = __spreadArray(__spreadArray([], createdProductIds, true), [doc.id], false);
+                    return [4 /*yield*/, req.payload.update({
+                            collection: "users",
+                            id: fullUser.id,
+                            data: {
+                                products: dataToUpdate,
+                            },
+                        })];
+                case 2:
+                    _b.sent();
+                    _b.label = 3;
+                case 3: return [2 /*return*/];
+            }
+        });
+    });
+};
+var createOrUpdateStripeProduct = function (args) { return __awaiter(void 0, void 0, void 0, function () {
+    var data, createdProduct, updated, data, updatedProduct, updated;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                if (!(args.operation === "create")) return [3 /*break*/, 2];
+                data = args.data;
+                return [4 /*yield*/, stripe_1.stripe.products.create({
+                        name: data.name,
+                        default_price_data: {
+                            currency: "USD",
+                            unit_amount: Math.round(data.price * 100),
+                        },
+                    })];
+            case 1:
+                createdProduct = _a.sent();
+                updated = __assign(__assign({}, data), { stripeId: createdProduct.id, priceId: createdProduct.default_price });
+                return [2 /*return*/, updated];
+            case 2:
+                if (!(args.operation === "update")) return [3 /*break*/, 4];
+                data = args.data;
+                return [4 /*yield*/, stripe_1.stripe.products.update(data.stripeId, {
+                        name: data.name,
+                        default_price: data.priceId,
+                    })];
+            case 3:
+                updatedProduct = _a.sent();
+                updated = __assign(__assign({}, data), { stripeId: updatedProduct.id, priceId: updatedProduct.default_price });
+                return [2 /*return*/, updated];
+            case 4: return [2 /*return*/];
+        }
+    });
+}); };
+var isAdminOrHasAccess = function () {
+    return function (_a) {
+        var _user = _a.req.user;
+        var user = _user;
+        if (!user)
+            return false;
+        if (user.role === "admin")
+            return true;
+        var userProductIds = (user.products || []).reduce(function (acc, product) {
+            if (!product)
+                return acc;
+            if (typeof product === "string")
+                acc.push(product);
+            else
+                acc.push(product.id);
+            return acc;
+        }, []);
+        return {
+            id: {
+                in: userProductIds,
+            },
+        };
+    };
+};
 exports.Products = {
     slug: "products",
     admin: {
         useAsTitle: "name",
     },
-    access: {},
+    access: {
+        read: isAdminOrHasAccess(),
+        update: isAdminOrHasAccess(),
+        delete: isAdminOrHasAccess(),
+    },
     hooks: {
-        beforeChange: [
-            addUser,
-            function (args) { return __awaiter(void 0, void 0, void 0, function () {
-                var data, createdProduct, updated, data, updatedProduct, updated;
-                return __generator(this, function (_a) {
-                    switch (_a.label) {
-                        case 0:
-                            if (!(args.operation === "create")) return [3 /*break*/, 2];
-                            data = args.data;
-                            return [4 /*yield*/, stripe_1.stripe.products.create({
-                                    name: data.name,
-                                    default_price_data: {
-                                        currency: "USD",
-                                        unit_amount: Math.round(data.price * 100),
-                                    },
-                                })];
-                        case 1:
-                            createdProduct = _a.sent();
-                            updated = __assign(__assign({}, data), { stripeId: createdProduct.id, priceId: createdProduct.default_price });
-                            return [2 /*return*/, updated];
-                        case 2:
-                            if (!(args.operation === "update")) return [3 /*break*/, 4];
-                            data = args.data;
-                            return [4 /*yield*/, stripe_1.stripe.products.update(data.stripeId, {
-                                    name: data.name,
-                                    default_price: data.priceId,
-                                })];
-                        case 3:
-                            updatedProduct = _a.sent();
-                            updated = __assign(__assign({}, data), { stripeId: updatedProduct.id, priceId: updatedProduct.default_price });
-                            return [2 /*return*/, updated];
-                        case 4: return [2 /*return*/];
-                    }
-                });
-            }); },
-        ],
+        beforeChange: [addUser, createOrUpdateStripeProduct],
+        afterChange: [syncUser],
     },
     fields: [
         {
--- next.config.js
+++ next.config.js
@@ -1,15 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        hostname: "localhost",
-        pathname: "**",
-        port: "3000",
-        protocol: "http",
-      },
-    ],
-  },
+		remotePatterns: [
+			{
+				protocol: "http",
+				hostname: "localhost",
+			},
+			{
+				protocol: "https",
+				hostname: "tutorial-marketplace-virid.vercel.app",
+			},
+		],
+	},
 };
 
 module.exports = nextConfig;
